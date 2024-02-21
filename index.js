5a0f2e5dd7637a71c9ebe993b009bb94b353a3d5
RaunakShrivastwa <kumarraunak4201@gmail.com>
2024-02-21 12:04:42 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(2000,()=>{
-    console.log("Server is Running ....");
+app.listen(PORT,(err)=>{
+    if(err){
+        return console.log("There  is Error ",err);
+    }
+
+    console.log(`Server Up and Running at PORT ${PORT}`);
 })
